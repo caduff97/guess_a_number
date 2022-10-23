438844ddfbd5dc52cf725f3b8f4b6afd779044c4
Carlos Eduardo <carloseduardo@MacBook-Air-de-Carlos.local>
2022-10-23 12:06:01 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# madlibs
+
+It's a simple guess a number game. You can play two different games. The first one, you try to guess a number set by the machine. The second, the machine to guess your number.
+
+## Installation
+
+Clone the repository using the following command.
+
+```bash
+git clone https://github.com/caduff97/guess_a_number
+```
+
+## Usage
+
+Make sure you have Python installed in your computer. Then run this command:
+
+```bash
+python3 main.py
+```
+
+Then answer the menu with "1" or "2" to set the game you want to play.
---
+++ main.py
@@ -0,0 +1,52 @@
+import random
+
+def guess(x):
+  random_number = random.randint(1, x)
+  guess = 0
+  while guess != random_number:
+    guess = int(input(f'Guess a number between 1 and {x}: '))
+    if guess < random_number:
+      print('Sorry, guess again. Too low.')
+    elif guess > random_number:
+      print('Sorry, guess again. Too high.')
+  
+  print(f'Yay, congrats! You have guessed the number {random_number} correctly.')
+    
+def computer_guess(x):
+  low = 1
+  high = x
+  feedback = ''
+  while feedback != 'c':
+
+    if low != 'c':
+      guess = random.randint(low,high)
+    else:
+      guess = low #could also be high bc low = high
+
+    feedback = input(f'Is {guess} too high(H), too low (L), or correct (C)? ').lower()
+    if feedback == 'h':
+      high = guess - 1
+    elif feedback == 'l':
+      low = guess + 1
+
+    if low > high:
+      print(f'Heey, you told me something wrong... You said is bigger than {low} and lower than {high}!?')
+      quit()
+
+  print(f'Yay! The computer guessed your number {guess}, correctly!')
+
+
+def main():
+  game = int(input(f'What game do you want to play? \n \
+  1 - Try to guess a number set by the computer; \n \
+  2 - The computer try to guess your number. \n Answer: '))
+  x = 1000
+  if game == 1:
+    guess(x)
+  elif game == 2:
+    computer_guess(x)
+  else:
+    print(f'Sorry, I don\'t understand your game.')
+    exit()
+
+main()
